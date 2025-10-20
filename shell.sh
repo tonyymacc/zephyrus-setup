a11324951e9649607b3c5b33b79b4c34361af525
Tony <tonyleo.mac@gmail.com>
2025-10-20 20:52:49 +0000

    Fix shell not changing to fish

--- shell.sh
+++ shell.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-chsh -s /usr/bin/fish
+# Ensure fish is in valid shells
+grep -q "/usr/bin/fish" /etc/shells || echo "/usr/bin/fish" | sudo tee -a /etc/shells
 
-echo "Shell done!"
+# Change shell
+sudo chsh -s /usr/bin/fish "$USER" || {
+  echo "Failed to change shell!"
+  exit 1
+}
+
+echo "Shell changed to fish! Re-login to apply."
