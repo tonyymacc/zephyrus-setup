8a7b45c0c9787fd4d4fb754c9a16ac9703af8e08
Tony <tonyleo.mac@gmail.com>
2025-10-20 20:09:05 +0000

    Fix repo not donwloading

--- preflight.sh
+++ preflight.sh
@@ -18,10 +18,7 @@ echo -e "\n$ansi_art\n"
 
 sudo pacman -Syu --noconfirm --needed git
 
-# Use custom repo if specified, otherwise default to basecamp/omarchy
-ZEPHYRUS_REPO="${ZEPHYRUS_REPO:-tonyymacc/zephyrus-setup}"
-
-echo -e "\nCloning from: https://github.com/${ZEPHYRUS_REPO}.git"
+echo -e "\nCloning from: https://github.com/tonyymacc/zephyrus-setup.git"
 rm -rf ~/zephyrus-setup
 git clone "https://github.com/${ZEPHYRUS_REPO}.git" ~/
 
