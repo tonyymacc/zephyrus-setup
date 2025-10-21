1dcd373815b1d5f688bd0b757ab8fdabde0ab0b9
Tony <tonyleo.mac@gmail.com>
2025-10-21 14:53:56 +0000

    Revision 3. Attempt fix yay

--- yay.sh
+++ yay.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-if ! command -v yay &>/dev/null; then
-  echo "Installing yay..."
-  sudo pacman -Syu --noconfirm
-  sudo pacman -S --needed --noconfirm git base-devel go
-  git clone https://aur.archlinux.org/yay.git
-  cd yay && makepkg -si
-  cd .. && rm -rf yay
-  echo "Yay installed!"
-else
-  echo "Yay already installed!"
-fi
+echo "Installing yay..."
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm git base-devel go
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
+cd .. && rm -rf yay
+echo "Yay installed!"
