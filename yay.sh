e4f238f6d923a6641b6b70a899a2000f22f3ecf8
Tony <tonyleo.mac@gmail.com>
2025-10-21 14:43:23 +0000

    Revision 3. Attempt fix yay

--- yay.sh
+++ yay.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Installing yay..."
-sudo pacman -S --needed --noconfirm git base-devel
+echo "Updating system and installing yay..."
+sudo pacman -Syu --needed --noconfirm git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
